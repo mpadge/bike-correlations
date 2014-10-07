fd9364fa381aab87077a0838500a3e0cb4c92921
mpadge <mark.padgham@email.com>
2014-10-07 11:32:09 +0200

    tmux start update

--- tmux_start.bash
+++ tmux_start.bash
@@ -28,6 +28,7 @@ tmux new-window -t $SESSION:3 -n python
 tmux select-window -t $SESSION:3
 tmux send-keys -t $SESSION:3 'vim router.py' C-m
 tmux send-keys -t $SESSION:3 ':' 'tabe getStDists.py' C-m
+tmux send-keys -t $SESSION:3 ':' 'tabe Utils.py' C-m
 tmux split-window -h
 tmux select-pane -t 0
 
@@ -37,6 +38,8 @@ tmux select-window -t $SESSION:4
 tmux send-keys -t $SESSION:4 'vim makefile' C-m
 tmux send-keys -t $SESSION:4 ':' 'tabe .travis.yml' C-m
 tmux split-window -h
+# Environmental variable only held in local tmux environment = that pane.
+tmux send-keys -t $SESSION:4 "GD='https://github.com/mpadge/bike-correlations'" C-m
 tmux select-pane -t 0
 
 tmux select-window -t $SESSION:3

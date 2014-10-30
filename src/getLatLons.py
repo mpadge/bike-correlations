62888803a0655b825f2f60e5247877ff2d580a78
mpadge <mark.padgham@email.com>
2014-10-30 09:48:51 +0100

    getLatLons appends new stations to old lists for both cities

--- src/getLatLons.py
+++ src/getLatLons.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python
 
-# Extracts lat-lons for all hire bicycle stations.  The coordinates for
-# citibikenyc are automatically downloaded from
-# http://www.citibikenyc.com/stations/json/
-#
-# London is more complicated for 2 reasons. First, the webpage is https, and
-# importing in python requires ssl and is thus fraught with OS system
-# differences and unreliable routines in urllib. To circumvent problems, this
-# script has to be run on the html source downloaded from
-# https://web.barclayscyclehire.tfl.gov.uk/maps/
-# which should be titled "maps.htm". 
-#
-# A second problem is that the tfl page only lists currently operating stations,
-# and so changes regularly, and will generally not included all historical
-# stations. (Currently closed stations are listed at
+# Extracts lat-lons for all hire bicycle stations.  Both London and NYC only
+# list currently operating stations, so station lists change regularly, and each
+# will generally not included all historical stations. (Currently closed
+# stations in London are listed at
 # http://www.tfl.gov.uk/modes/cycling/barclays-cycle-hire/find-a-docking-station)
 # This script can thus be run multiple times, with each newly appearing station
 # appended to any pre-existing ones.
+#
+# The coordinates for citibikenyc are automatically downloaded from
+# http://www.citibikenyc.com/stations/json/
+#
+# London is more complicated because the webpage is https, and importing in
+# python requires ssl and is thus fraught with OS system differences and
+# unreliable routines in urllib. To circumvent problems, this script has to be
+# run on the html source downloaded from
+# https://web.barclayscyclehire.tfl.gov.uk/maps/ which should be titled
+# "maps.htm". 
 
 import sys, getopt, re, urllib2, json, os
 from bs4 import BeautifulSoup
 import pandas as pd
 
+def rootDir ():
+        return os.getcwd ().split ('/src')[0]
+
 def getLondon ():
-    f = open ('../data/maps.htm', 'r')
+    f = open (rootDir() + '/data/maps.htm')
     page = f.read ()
     f.close ()
     soup = BeautifulSoup (page)
@@ -34,10 +37,10 @@ def getLondon ():
     # list is a single bs4 object, which is then converted to a string
     list = list.find (text=True).encode ('utf-8') 
     list = list.split ('station=')
-    fname = '../data/station_latlons_london.txt'
     outs = ('id', 'lat', 'long', 'name')
     # Values from maps.htm are appended to any existing values, and stripped
     # back to unique values with .drop_duplicates() afterwards
+    fname = rootDir() + '/data/station_latlons_london.txt'
     if os.path.lexists (fname): 
         df = pd.read_csv (fname)
         df.columns = outs # necessary because to_csv inserts spaces
@@ -45,7 +48,7 @@ def getLondon ():
         df = pd.DataFrame(columns=outs)
     count = 0
     for li in list:
-        if li.find ("name") > 0:
+        if li.find ('name') > 0:
             # This regex extracts all text NOT contained within double quotes, which
             # thus contains all field names of the javascript source
             linames = re.findall ('(?:^|")([^"]*)(?:$|")', li)
@@ -61,33 +64,33 @@ def getLondon ():
                 df = pd.concat ([df, pd.DataFrame (lidat, index=outs).transpose()])
                 count = count + 1
 
-    df = df.drop_duplicates ()
+    df = df.drop_duplicates (cols=['id','name']).sort('id')
     df.to_csv (fname, index=False)
-    ids = df["id"].convert_objects(convert_numeric=True)
-    print "%s London stations in [%s - %s] written to %s" % (count, ids.min (),
-        ids.max (), fname)
+    ids = df['id'].convert_objects(convert_numeric=True)
+    print '%s London stations in [%s - %s] written to %s' % (count, ids.min (),
+            ids.max (), fname)
 
 def getNYC ():
     url = 'http://www.citibikenyc.com/stations/json/'
     content = urllib2.urlopen (url).read ()
     data = json.loads (content)
     data = data['stationBeanList']
-    fname = '../data/station_latlons_nyc.txt'
-    f = open (fname, 'w')
-    f.write ('id, lat, long, name\n')
-    count = 0
-    minstn = 9999
-    maxstn = 0
+    outs = ('id', 'lat', 'long', 'name')
+    fname = rootDir() + '/data/station_latlons_nyc.txt'
+    if os.path.lexists (fname): 
+        df = pd.read_csv (fname)
+        df.columns = outs # necessary because to_csv inserts spaces
+    else:
+        df = pd.DataFrame(columns=outs)
     for st in data:
-        f.write (str (st['id']) + ", " + str (st['latitude']) + ", ")
-        f.write (str (st['longitude']) + ", " + str (st['stationName']) + '\n')
-        id = int (st['id'])
-        if id < minstn: minstn = id
-        elif id > maxstn: maxstn = id
-        count += 1
-    print count, 'NYC stations in [', minstn, ',', maxstn,
-    print '] written to ', fname
-    f.close ()
+        st1 = [st['id'], st['latitude'], st['longitude'], st['stationName']]
+        df = pd.concat ([df, pd.DataFrame (st1, index=outs).transpose()])
+    
+    df = df.drop_duplicates (cols='id').sort('id')
+    df.to_csv (fname, index=False)
+    ids = df['id'].convert_objects(convert_numeric=True)
+    print '%s NYC stations in [%s - %s] written to %s' % (df.count()[0], ids.min
+            (), ids.max (), fname)
 
 def num (s):
     try:
